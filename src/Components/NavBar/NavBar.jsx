a9403e3a9c845eeef16763a5c25395b19b122ad7
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-13 00:36:13 +0600

    added new files

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, MonitorX } from 'lucide-react';
 // import Link from './Link';
 
 const navigationData = [
@@ -31,14 +32,31 @@ const navigationData = [
 ]
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false)
+    const links = navigationData.map(route => <Link key={route.id} route={route} />)
+
     return (
-        <div>
-            <ul className='flex gap-5'>
+        <nav className='flex justify-between lg:px-10 px-2 py-3'>
+            <div className='flex gap-2 items-center' >
+                <span onClick={() => setOpen(!open)}>
+                    {open ? <MonitorX /> : <Menu className='lg:hidden' />}
+                </span>
+                <ul className={`lg:hidden absolute ${open ? 'top-12' : 'top-64'} duration-75`} >
+                    {
+                        links
+                    }
+                </ul>
+                <h2>My Navbar</h2>
+            </div>
+            <ul className='lg:flex hidden gap-5'>
                 {
-                    navigationData.map(route => <Link key={route.id} route={route} />)
+                    links
                 }
             </ul>
-        </div>
+            <button>
+                Sign In
+            </button>
+        </nav>
     );
 };
 
